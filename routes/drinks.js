2ef2bd4e414c521852d6f5eca64af2bacef5401d
MohamedAbuBasith <mohamedabu.basith@grootan.com>
2022-07-03 17:15:43 +0530

    Drinks product added

--- routes/drinks.js
+++ routes/drinks.js
@@ -83,7 +83,7 @@ router.get("/", (req, res, next) => {
         .then(docs => {
             const response = {
                 count: docs.length,
-                products: docs.map(doc => {
+                drinksroduct: docs.map(doc => {
                     return {
                         name: doc.name,
                         price: doc.price,
