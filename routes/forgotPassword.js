f9e0ca08d55477a16c24a80dcfa28284f6a0c9d5
Mohamed Abu Basith <mohamedabu.basith@kobil.com>
2022-07-23 21:06:07 +0530

    some changes

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -71,7 +71,7 @@ router.post('/', async (req, res, next) => {
                                    <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Hayat Store</a>
                                  </div>
                                  <p style="font-size:1.1em">Hi ${data.username},</p>
-                                 <p>Thank you for choosing the Hayat. Use the following OTP to complete your forgot password procedures. OTP is valid for 5 minutes</p>
+                                 <p>Thank you for choosing the Hayat. Use the following OTP to complete your forgot password procedures. OTP is valid for 2 minutes</p>
                                  <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${newOtpValue}</h2>
                                  <p style="font-size:0.9em;">Regards,<br />Hayat Store</p>
                                  <hr style="border:none;border-top:1px solid #eee" />
