05785bd3b126ea7ac49273ebe0806583cd2b2bd2
Mohamed Abu Basith <mohamedabu.basith@kobil.com>
2024-08-31 13:38:27 +0530

    CHG: GOOGLE_ID changed and server.js improved.

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 const http = require('http');
 const app = require('./app');
-const port = 8080; // Replace with your desired port
+require('dotenv').config(); // Ensure environment variables are loaded
+
+// Set port to either environment variable PORT or default to 8080
+const port = process.env.PORT || 8080;
 const server = http.createServer(app);
-require('dotenv').config();
 const mongoURI = process.env.MONGODB_URI;
 
 // Connect to MongoDB
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch(error => console.error('MongoDB connection error:', error));
 
 mongoose.Promise = global.Promise;
 
 server.listen(port, function () {
-  console.log('Server is started on http://127.0.0.1:' + port);
+  console.log(`Server is started on http://127.0.0.1:${port}`);
 });
